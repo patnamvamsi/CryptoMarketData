91ffb0717a4bebeb287884ea6ffac2406d9bc4c1
Vamsi Patnam <patnamvamsi@gmail.com>
2022-11-01 23:41:53 +1100

    Keep the marketdata DB upto date. update historical data as well as start wss stream to catpure market data.

--- app/main.py
+++ app/main.py
@@ -31,7 +31,9 @@ class historicaldata_post(BaseModel):
 def landing():
     return "welcome to the crypto market data module"
 
-
+''' The current issue with streaming is:
+    by the time the historical data procedure starts and finishes fetching data,
+    it takes few minutes, and it causes break between historical and streaming data. '''
 def stream_kline_data():
     stream_market_data = StreamKLineData()
     print ("Started thread")
@@ -48,7 +50,7 @@ def fetch_historical_data():
 def app_startup():
     scheduler = BackgroundScheduler()
     scheduler.add_job(fetch_historical_data)
-    scheduler.add_job(stream_kline_data)
+    #scheduler.add_job(stream_kline_data)  
     scheduler.start()
 
 
@@ -86,15 +88,8 @@ def update_market_data(symbol: str):
     c = h.BinanceDownloader()
     return c.fetch_historical_data(symbol)
 
-# BinanceDownloader()._fetch_all_historical_data() -- to update all active symbols
 
 '''
-apis:
-1. get symbol with date range, default 1min
-2. get symbol with date range and frequency (1m, 5m, 15m etc)
-3. get streaming data -- which protocol to use?
-    
-
 future apis
 accept the source binance or coinbase etc, default to binance
 1. Design  decision -- how to stream data with min lag -- required for live trading, not now
@@ -109,45 +104,3 @@ https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
 '''
 
 ''' iki When to use get and when to use post'''
-
-''' 
-keep track of all the symbol pairs 
-    1. as soon as the server starts up
-    2. every 24 hrs
-    3. on invoking the API
-        i. use get method to invoke api and return any new symbols found
-
-if any new symbol is listed in binance,
- 1. update the symbol table 
- 2. create a new table for the symbol
- 3. start fetching the data
- 
- 
- '''
-
-'''
-write a genric function to create database tables automatically for coin pairs
-create index on the table on timestamp column
-table name format <source>_<coinpair> eg: BINANCE_XRPAUD
-'''
-
-'''
-make config:
-1. if csv dump is available , use that
-2. if not , download from the website
-3. Run a bg task to keep looping through each symbol, find out the last updated timestamp and fetch till current timestamp
-'''
-
-'''
-Create multiple processes to fetch symbols (upto 5) paralelly from the source and store in the database
-Make it scalable if the number of symbols are huge -- iki how to do this? docker based or something else?
-'''
-
-'''
-create and setup a DB for storing the data
-'''
-
-'''
-If any DB errors\ unable to connect to DB etc , send error info to the webserver, but keep the service running
-Same for any other exceptions\ errors
-'''
